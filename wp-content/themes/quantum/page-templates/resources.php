a5aea1d236eadec4572ee1dcb03adb804a43a843
Sam <floatingdino@gmail.com>
2016-07-06 20:13:51 +1000

    early resources page styles

--- wp-content/themes/quantum/page-templates/resources.php
+++ wp-content/themes/quantum/page-templates/resources.php
@@ -5,9 +5,24 @@ Template Name: Resources
 get_header(); ?>
 
 <?php do_action( 'foundationpress_before_content' ); ?>
-<?php while ( have_posts() ) : the_post(); ?>
-<h1></h1>
-<?php endwhile;?>
+<main role="main">
+	<h1> <?php the_title(); ?> </h1>
+	<ul class="filters">
+		<li class="highlight">News</li><li>Whitepapers</li><li>Resources</li><li>Events</li>
+	</ul>
+	<section id="articles">
+		<?php while ( have_posts() ) : the_post(); ?>
+			<article class="news">
+				<img class="hero">
+				<div class="content">
+					<p class="article-details"><strong>News</strong> / 06 Jun 2016</p>
+					<h2>How to Manage Your Trust Distribution This EOFY</h2>
+					<p>We're a new breed of advisors who work with business who work with business owners...</p>
+				</div>
+			</article>
+		<?php endwhile;?>
+	</section>
+</main>
 <?php do_action( 'foundationpress_after_content' ); ?>
 
 
