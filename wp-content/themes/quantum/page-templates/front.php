8474327513d3d33c6f7d32c7c3c1422ba837867a
Sam <floatingdino@gmail.com>
2016-07-22 15:44:49 +1000

    final draft 1

--- wp-content/themes/quantum/page-templates/front.php
+++ wp-content/themes/quantum/page-templates/front.php
@@ -17,8 +17,8 @@ get_header(); ?>
 	<?php if(have_rows('blocks')):?>
 		<section class="top-blocks">
 			<?php while(have_rows('blocks')):the_row();?>
-				<article class="block" style="background-image:url(<?php the_sub_field('background_image');?>)">
-					<div>
+				<article class="block">
+					<div style="background-image:url(<?php the_sub_field('background_image');?>)">
 						<a href="<?php the_sub_field('link'); ?>">
 						<h2><?php the_sub_field('tag');?></h2>
 						<h3><?php the_sub_field('title');?> <i class="triangle"></i></h3>
@@ -43,9 +43,9 @@ get_header(); ?>
 				setup_postdata($post);
 				?>
 				<article class="person">
-					<a href="<?php the_permalink; ?>">
-					<div class="headshot">
-						<?php the_post_thumbnail(); ?>
+					<a href="<?php the_permalink(); ?>">
+					<div class="headshot" style="background-image:url(<?php the_post_thumbnail_url('medium');?>)">
+						<?php //the_post_thumbnail(); ?>
 					</div>
 					<h4><?php the_title(); ?></h4>
 					</a>
@@ -54,9 +54,34 @@ get_header(); ?>
 			<div class="clear"></div>
 	</section>
 	<?php endif;?>
+
+	<section class="resources">
+		<h1>Resources</h1>
+		<div class="container">
+		<?php $args = array('posts_per_page'=>4);
+		$loop = new WP_Query($args);
+		while($loop->have_posts()):$loop->the_post();	?>
+		<article class="<?php echo get_the_category()[0]->slug;?> item">
+			<div class="article-body">
+				<?php
+				if(has_post_thumbnail()){
+						the_post_thumbnail();
+				}
+				?>
+				<div class="content">
+					<p class="article-details"><strong><?php echo get_the_category()[0]->name;?></strong> / <?php echo get_the_date(get_option('date_format'));?></p>
+					<h3><?php the_title();?></h3>
+					<p><?php the_excerpt();?></p>
+				</div>
+			</div>
+		</article>
+	<?php endwhile; wp_reset_postdata();?>
+	<div class="clear"></div>
+	<a class="button" href="/resources">View More</a>
+	</div>
+	</section>
 <?php endwhile;?>
 <?php do_action( 'foundationpress_after_content' ); ?>
 </main>
-
 
 <?php get_footer();
